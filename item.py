c02110a67db379c32413a5584589a8d9ee78de6c
BirdsArentRea1 <146360250+BirdsArentRea1@users.noreply.github.com>
2024-05-16 10:49:23 -0600

    Update item.py

--- item.py
+++ item.py
@@ -10,8 +10,20 @@ class item:
 
     def draw(self,screen):
         if self.collected == False:
-            pygame.draw.circle(screen, (250, 0, 0), (self.x, self.y), 10)
-            pygame.draw.circle(screen, (250, 250, 0), (self.x, self.y), 5)
+            if self.type == "potion":
+                pygame.draw.circle(screen, (250, 0, 0), (self.x, self.y), 10)
+                pygame.draw.circle(screen, (250, 250, 0), (self.x, self.y), 5)
+            elif self.type == "ring":
+                pygame.draw.circle(screen, (250, 0, 0), (self.x, self.y), 10)
+                pygame.draw.circle(screen, (0, 250, 0), (self.x, self.y), 5)
+                
+        if self.collected == True:
+            if self.type == "potion":
+                pygame.draw.circle(screen, (250, 0, 0), (200,750), 10)
+                pygame.draw.circle(screen, (250, 250, 0), (200,750), 5)
+            elif self.type == "ring":
+                pygame.draw.circle(screen, (250, 0, 0), (275,775), 10)
+                pygame.draw.circle(screen, (0, 250, 0), (275,775), 5)
             
 
     def collect(self):
