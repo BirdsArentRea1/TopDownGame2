a2f2026e5c45af28e4a4be91e683ddc73825b0e7
BirdsArentRea1 <146360250+BirdsArentRea1@users.noreply.github.com>
2024-05-22 10:35:18 -0600

    Update item.py

--- item.py
+++ item.py
@@ -1,5 +1,7 @@
 import pygame
 
+potion = pygame.image.load('imgs/potion.png')
+
 class item:
     def __init__ (self, x, y, type):
         self.collected = False
@@ -7,20 +9,17 @@ class item:
         self.y = y
         self.type = type
 
-
     def draw(self,screen):
         if self.collected == False:
             if self.type == "potion":
-                pygame.draw.circle(screen, (250, 0, 0), (self.x, self.y), 10)
-                pygame.draw.circle(screen, (250, 250, 0), (self.x, self.y), 5)
+                screen.blit(potion, (self.x , self.y ), (0, 0, 50, 50))
             elif self.type == "ring":
                 pygame.draw.circle(screen, (250, 0, 0), (self.x, self.y), 10)
                 pygame.draw.circle(screen, (0, 250, 0), (self.x, self.y), 5)
                 
         if self.collected == True:
             if self.type == "potion":
-                pygame.draw.circle(screen, (250, 0, 0), (200,750), 10)
-                pygame.draw.circle(screen, (250, 250, 0), (200,750), 5)
+                screen.blit(potion, (200, 755 ), (0, 0, 50, 50))
             elif self.type == "ring":
                 pygame.draw.circle(screen, (250, 0, 0), (275,775), 10)
                 pygame.draw.circle(screen, (0, 250, 0), (275,775), 5)
